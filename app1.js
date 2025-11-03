227f229b79cbc34800f597badceeab0468feb91a
nadiabarbara <nadiabarbara10@gmail.com>
2025-11-03 19:51:42 +0200

    Delete app1.js

--- app1.js
+++
@@ -1,9 +0,0 @@
-//code written by  & Nadia Razi 
-const num = 123//מספר כלשהוא
-
-//בודקים אם התנאים שדרושים מתקיימים ואחרכך מדפיסים את התוצאה
-const isDivisible2 = num%2===0;
-const isDivisible3 = num % 3 === 0;
-const isDivisible5 = num % 5 === 0;
-const result =Number(isDivisible2) + Number(isDivisible3) + Number(isDivisible5);
-console.log(result);
